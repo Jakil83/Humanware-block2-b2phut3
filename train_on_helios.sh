97c542003e4cb04aa032d2b8fbb3ec66a57c08bb
Jakil83 <jakil.slimi@gmail.com>
2019-03-07 17:49:32 -0500

    Updated scripts to run on helios. This fixes #3, and fixes #4.

--- train_on_helios.sh
+++ train_on_helios.sh
@@ -3,14 +3,14 @@
 #PBS -l nodes=1:gpus=1
 #PBS -l advres=MILA2019
 #PBS -l feature=k80
-#PBS -l walltime=00:30:00
+#PBS -l walltime=01:00:00
 
 # set the working directory to where the job is launched
 cd "${PBS_O_WORKDIR}"
 
-export ROOT_DIR=$HOME'/Humanware-block2-b2phut3/'
+export ROOT_DIR=$HOME'/Humanware-block2-b2phut3'
 export SVHN_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/SVHN'
-export DATA_DIR=[$SVHN_DIR/train, $SVHN_DIR/extra]
-export METADATA_FILENAME=[$SVHN_DIR/train_metadata.pkl, $SVHN_DIR/extra_metadata.pkl]
+export DATA_DIR='$SVHN_DIR/train $SVHN_DIR/extra'
+export METADATA_FILENAME='$SVHN_DIR/train_metadata.pkl $SVHN_DIR/extra_metadata.pkl'
 
-s_exec python $ROOT_DIR'/train.py' --dataset_dir=$TMP_DATA_DIR --metadata_filename=$METADATA_FILENAME --results_dir=$ROOT_DIR/results --cfg $ROOT_DIR/config/base_config.yml
+s_exec python $ROOT_DIR'/train.py' --dataset_dir $DATA_DIR --metadata_filename $METADATA_FILENAME --results_dir $ROOT_DIR/results --cfg $ROOT_DIR/config/base_config.yml
