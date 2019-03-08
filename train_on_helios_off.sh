88320bc8bd09f89f36716b616597533de05ffd06
Moneim <aalkhalil155@gmail.com>
2019-03-07 22:20:08 -0500

    add paper model arch , add train on helios during off hours

--- train_on_helios_off.sh
+++ train_on_helios_off.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #PBS -A colosse-users
 #PBS -l nodes=1:gpus=1
-#PBS -l advres=MILA2019
-#PBS -l feature=k80
 #PBS -l walltime=01:00:00
 
 # set the working directory to where the job is launched
