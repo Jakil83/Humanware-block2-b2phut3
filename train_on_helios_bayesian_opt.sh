3dd382007a6abd0a2b0cd9bb7d93c8b3cbbbce7f
Julien Allard <julien.allard@umontreal.ca>
2019-03-07 19:30:56 -0500

    Updated script to run on helios

--- train_on_helios_bayesian_opt.sh
+++ train_on_helios_bayesian_opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -A colosse-users
 #PBS -l nodes=1:gpus=1
-#PBS -l walltime=04:30:00
+#PBS -l walltime=012:00:00
 # set the working directory to where the job is launched
 cd "${PBS_O_WORKDIR}"
 
